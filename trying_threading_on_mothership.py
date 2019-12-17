a3f08e0ad91c82ad94b50d7d51f87f9a29800e0d
Aakash Sinha <aakashsinha46@gmail.com>
2019-12-17 17:52:22 +0530

    threading is on beta

--- trying_threading_on_mothership.py
+++ trying_threading_on_mothership.py
@@ -1,11 +1,18 @@
 from get_soup import get_soup_html
+from threading import Thread
 import pprint
 
-def get_mothership_politics_news(soup=None):
+def get_mothership_news_links(soup=None):
    news = []
    for item in soup.find_all("div", attrs={"class": "ind-article"})[1:]:
       news.append({'link': item.find('a')['href']})
-   
+   #get_mothership_news(news)
+   t2 = Thread(target=get_mothership_news, args=(news,)) 
+   t2.start()
+   t2.join()
+   print(news)
+ 
+def get_mothership_news(news=None):
    for item in news:
       soup = get_soup_html(item['link'])
 
@@ -20,11 +27,12 @@ def get_mothership_politics_news(soup=None):
       'heading': heading,
       'desc' : summary
       })
-  
-   print(news)
 
 if __name__ == "__main__":
    categorys_on_mothership = ['parliament/', 'tech/', 'lifestyle/', 'lifestyle/celebrity/']
    for category in categorys_on_mothership:
       html = get_soup_html("https://mothership.sg/category/{link}".format(link=category))
-      get_mothership_politics_news(html)
+      #get_mothership_news_links(html)
+      t1 = Thread(target=get_mothership_news_links, args=(html,)) 
+      t1.start() 
+   
